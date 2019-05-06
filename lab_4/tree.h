0556f5fb9cf43facacb95902edf358f98b02cbcc
hryhorchuk117 <47106030+hryhorchuk117@users.noreply.github.com>
2019-05-06 12:47:27 +0300

    deleting && binary tree added

--- lab_4/tree.h
+++ lab_4/tree.h
@@ -5,18 +5,30 @@
 #include <iostream>
 
 struct node {
-    explicit node(int new_data) {
-        this->data = new_data;
-        this->next = {nullptr};
+    explicit node(int newData) {
+        this->data = newData;
+        this->next = {};
     }
 
-    explicit node(int new_data, const std::vector<node*>& new_next) {
-        this->data = new_data;
-        this->next = new_next;
+    explicit node(int newData, const std::vector<node*>& newNext) {
+        this->data = newData;
+        this->next = newNext;
     }
 
     int data;
     std::vector<node*> next;
+};
+
+struct binaryNode {
+    explicit binaryNode(int newData) {
+        this->data = newData;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+
+    int data;
+    binaryNode* left;
+    binaryNode* right;
 };
 
 int randomInt(int begin, int end) {
@@ -31,16 +43,26 @@ bool randomTrue(double p) {
     return p > rand;
 }
 
-void addNode(int new_data, double p, node* curr) {
+template <class node>
+void addNode(int newData, double p, node* curr) {
     if (randomTrue(p) || curr->next.empty()) {
-        node* new_node = new node(new_data);
-        curr->next.push_back(new_node);
+        node* newNode = new node(newData);
+        curr->next.push_back(newNode);
     } else {
         int k = randomInt(0, curr->next.size());
-        addNode(new_data, p, curr->next[k]);
+        addNode(newData, p, curr->next[k]);
     }
 }
 
+template <class node>
+void addTree(node* add, double p, node* head) {
+    addNode(add->data, p, head);
+    for (auto item : add->next) {
+        addTree(item, p, head);
+    }
+}
+
+template <class node>
 void printTree(node* head) {
     std::cout << head->data << '(';
     for (auto item : head->next) {
@@ -49,3 +71,71 @@ void printTree(node* head) {
     std::cout << ')';
 }
 
+template <class node>
+std::vector<node*> findNode(int findData, node* head, std::vector<node*>& res) {
+    if (head->data == findData) {
+        res.push_back(head);
+    }
+    for (auto item : head->next) {
+        findNode(findData, item, res);
+    }
+    return res;
+}
+
+template <class node>
+void deletePointer(node* head, node* del) {
+    if (head == del) {
+        return;
+    }
+    for (auto it = begin(head->next); it <end(head->next); ++it) {
+        if (*it == del) {
+            head->next.erase(it);
+            break;
+        }
+    }
+
+    for (auto item : head->next) {
+        deletePointer(item, del);
+    }
+}
+
+template <class node>
+void deleteNode(node* head, int delData, double p) {
+    if (head->data == delData) {
+        node* newHead = new node(head->next[0]->data);
+        for (int i = 1; i < head->next.size(); ++i) {
+            addTree(head->next[i], p, newHead);
+        }
+        head = newHead;
+    }
+
+    std::vector<node*> res = findNode(delData, head, res);
+    for (auto del : res) {
+        deletePointer(head, del);
+        for (auto item : del->next) {
+            addTree(item, p, head);
+        }
+    }
+}
+
+void addBinary(int add, binaryNode* head) {
+
+    if (add < head->data && head->left == nullptr) {
+        auto* lft = new binaryNode(add);
+        head->left = lft;
+        return;
+    }
+
+    if (add >= head->data && head->right == nullptr) {
+        auto* rght = new binaryNode(add);
+        head->right = rght;
+        return;
+    }
+
+    if (add < head->data) {
+        addBinary(add, head->left);
+        return;
+    }
+
+    addBinary(add, head->right);
+}
